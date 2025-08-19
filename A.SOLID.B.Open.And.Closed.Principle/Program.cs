e2b7063a08cfa643597129b46caadcd9b47fd565
DaoChaShao <27882272@qq.com>
2025-08-19 16:51:38 +0800

    feat: add Program class to demonstrate product initialization and filtering by colour and size

---
+++ A.SOLID.B.Open.And.Closed.Principle/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace A.SOLID.B.Open.And.Closed.Principle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Initialisation
+
+        var random = new Random();
+        var colours = new List<Colour> { Colour.Red, Colour.Yellow, Colour.Blue };
+        var sizes = new List<Size> { Size.Large, Size.Medium, Size.Small };
+        var products = new List<string> { "bike", "cup", "bag" };
+
+        var cart = new List<Product>();
+
+        #endregion
+
+        #region OOP
+
+        var amountProducts = random.Next(5, 11);
+        for (var i = 0; i < amountProducts; i++)
+        {
+            var product = new Product(
+                products[random.Next(products.Count)],
+                colours[random.Next(colours.Count)],
+                sizes[random.Next(sizes.Count)]
+            );
+            Console.WriteLine($"Product {i + 1}: {product}");
+            cart.Add(product);
+        }
+
+        Console.WriteLine($"The cart contains {cart.Count} products.");
+
+        #endregion
+
+        #region OCP - Colour
+
+        var selectedColour = colours[random.Next(colours.Count)];
+
+        var specificationColour = new SpecificationColour(selectedColour);
+        var filter = new Filter();
+
+        var productsColour = filter.Products(cart, specificationColour);
+        foreach (var product in productsColour)
+        {
+            Console.WriteLine($"The {selectedColour} product is: {product}");
+        }
+
+        #endregion
+
+        #region OCP - Size
+
+        var selectedSize = sizes[random.Next(sizes.Count)];
+
+        var specificationSize = new SpecificationSize(selectedSize);
+
+        var productsSize = filter.Products(cart, specificationSize);
+        foreach (var product in productsSize)
+        {
+            Console.WriteLine($"The {selectedSize} product is: {product}");
+        }
+
+        #endregion
+    }
+}
