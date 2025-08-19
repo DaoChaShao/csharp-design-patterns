3418577714a9a21e81a000d5f514f9099cf0fb0f
DaoChaShao <27882272@qq.com>
2025-08-19 13:57:52 +0800

    feat: implement journal recording and persistence in Main method

--- A.SOLID.A.Single.Responsibility.Principle /Program.cs
+++ A.SOLID.A.Single.Responsibility.Principle /Program.cs
@@ -1,9 +1,33 @@
-﻿namespace A.SOLID.A.Single.Responsibility.Principle;
+﻿using System.IO.Compression;
+
+namespace A.SOLID.A.Single.Responsibility.Principle;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region OOP
+
+        var journal = new Journal();
+        journal.Record("I cried today!");
+        journal.Record("I ate a delicious bug!");
+        journal.Record("I was kissed by a frog!");
+        Console.WriteLine(journal);
+
+        journal.Remove(journal, new Random().Next(1, journal.DictionaryCount + 1));
+        Console.WriteLine(journal);
+
+        #endregion
+
+        #region Persistence
+
+        var fileName = "journal";
+        StorageManager.SaveAsTextFile(journal, fileName);
+        Console.WriteLine("Journal saved to text file.");
+
+        StorageManager.LoadFromTextFile(fileName);
+        Console.WriteLine("Journal loaded from text file.");
+
+        #endregion
     }
 }
