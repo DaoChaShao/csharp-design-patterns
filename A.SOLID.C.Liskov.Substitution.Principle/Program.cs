187332f8945bffdaaa208587d3ba4d5bfd74fed8
DaoChaShao <27882272@qq.com>
2025-08-20 18:58:43 +0800

    feat: implement Main method to demonstrate Square class inheritance from Shape and Rectangle

---
+++ A.SOLID.C.Liskov.Substitution.Principle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace A.SOLID.C.Liskov.Substitution.Principle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Initialisation
+
+        var random = new Random();
+        var side = random.Next(1, 11);
+
+        #endregion
+
+        #region Square inherits Rectangle
+
+        var squareRectangle = new SquareFromRectangle(side);
+        Console.WriteLine(
+            $"The area of the square that inherits the Rectangle class with side {side} is {squareRectangle.Area()}"
+        );
+
+        #endregion
+
+        #region Square inherits Shape
+
+        var squareShape = new SquareFromShape(side);
+        Console.WriteLine(
+            $"The area of the square that inherits the Shape class with side {side} is {squareShape.Area()}"
+        );
+
+        #endregion
+    }
+}
