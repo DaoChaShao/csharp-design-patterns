9325f4c011f0723ba154f8272aee40d6f4f338ba
DaoChaShao <27882272@qq.com>
2025-08-21 13:44:28 +0800

    feat: implement Main method to demonstrate Law of Demeter usage

---
+++ B.Law.of.Demeter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace B.Law.of.Demeter;
+
+using B.Law.of.Demeter.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region LoD
+
+        var driver = new Driver();
+        var car = new Car();
+        Console.WriteLine(driver.Drive(car));
+
+        #endregion
+    }
+}
